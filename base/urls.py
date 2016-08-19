57d83907bbf941e9c337232947ff2fd19d8cdb33
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-08-19 14:36:14 -0400

    agregada url para la sección de inicio del sistema

---
+++ base/urls.py
@@ -0,0 +1,24 @@
+"""
+Sistema Estadístico Integral de Venezuela - (SEIVEN)
+
+Copyleft (@) 2015 CENDITEL nodo Mérida - https://mpv.cenditel.gob.ve/seiven
+"""
+## @namespace base.urls
+#
+# Contiene las urls del módulo base
+# @author Ing. Roldan Vargas (rvargas at cenditel.gob.ve)
+# @author <a href='http://www.cenditel.gob.ve'>Centro Nacional de Desarrollo e Investigación en Tecnologías Libres
+# (CENDITEL) nodo Mérida - Venezuela</a>
+# @copyright <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU Public License versión 2 (GPLv2)</a>
+from __future__ import unicode_literals
+from django.conf.urls import url, patterns
+
+
+urlpatterns = [
+    url(r'^inicio/$', 'base.views.inicio', name='inicio'),
+]
+
+
+## URLs de peticiones AJAX
+urlpatterns += [
+]
