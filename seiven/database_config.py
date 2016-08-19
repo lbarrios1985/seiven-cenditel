b9c0d69bfa7dc03377fba02f2684701f8b0ae726
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-08-19 08:47:36 -0400

    agregado archivo de configuración de la base de datos

---
+++ seiven/database_config.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+DATABASES_CONFIG = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<DB_NAME>',
+        'USER': '<DB_USER>',
+        'PASSWORD': '<DB_PASSWORD>',
+        'HOST': '<DB_SERVER>',
+        'PORT': '<DB_SERVER_PORT>',
+        'ATOMIC_REQUESTS': True, # Crea transacciones en cada peticion de la vista
+    }
+}
