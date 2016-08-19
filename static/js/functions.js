10c4f5fd04ab48d691235de6ecdc349fb79e332b
Ing. Roldan D. Vargas G <rvargas@cenditel.gob.ve>
2016-08-19 12:11:09 -0400

    agregada función para establecer la fortaleza de contraseñas

---
+++ static/js/functions.js
@@ -0,0 +1,34 @@
+/**
+ * @brief Función que mide la fortaleza de la contraseña y la muestra en pantalla
+ * @param password Cadena de carácteres con la contraseña indicada por el usuario
+ */
+function passwordStrength(password) {
+    var desc = new Array();
+    desc[0] = MSG_PASSWD_MUY_DEBIL;
+    desc[1] = MSG_PASSWD_DEBIL;
+    desc[2] = MSG_PASSWD_REGULAR;
+    desc[3] = MSG_PASSWD_BUENA;
+    desc[4] = MSG_PASSWD_FUERTE;
+    desc[5] = MSG_PASSWD_MUY_FUERTE;
+
+    var score = 0;
+
+    //if password bigger than 6 give 1 point
+    if (password.length > 6) score++;
+
+    //if password has both lower and uppercase characters give 1 point
+    if (( password.match(/[a-z]/) ) && ( password.match(/[A-Z]/) )) score++;
+
+    //if password has at least one number give 1 point
+    if (password.match(/\d+/)) score++;
+
+    //if password has at least one special caracther give 1 point
+    if (password.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/)) score++;
+
+    //if password bigger than 12 give another 1 point
+    if (password.length > 12) score++;
+
+    document.getElementById("passwordDescription").innerHTML = desc[score];
+    document.getElementById("passwordStrength").className = "strength" + score;
+    document.getElementById("passwordMeterId").value = score;
+}
